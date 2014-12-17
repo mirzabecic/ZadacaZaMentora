dcacac18566e794ce616a1d0f263f3fa45d8ba15
Haris Krajina <haris@atlantbh.com>
2014-12-17 10:42:14 +0100

    CR su code review commentari

--- AutorTest.java
+++ AutorTest.java
@@ -1,10 +1,26 @@
 
 public class AutorTest {
 	public static void main(String[] args) {
+
+		/*
+		* CR: Nisi komitao clasu Autor
+		*/
+
+		/*
+		* CR: Voditi racuna o 'spaceovima' tako npr linija ispod
+		*
+		* Autor mirza = new Autor("mirza", "mirza.sdsads@hotmail.com", "m");
+		*
+		* Znakovi:
+		*
+		* =  -> Sa obje strane ide SPACE
+		* ,  -> Nakon zareza uvijek ide SPACE
+		*/
+
 		Autor mirza= new Autor("mirza", " mirza.sdsads@hotmail.com", "m");
 		Autor davor= new Autor("davor", "davor.asdsa@hotmail.com","z");
 		davor.setIme("hafir");
-	
+
 		Knjiga prva= new Knjiga();
 		System.out.println(prva);
 		System.out.println(mirza);
